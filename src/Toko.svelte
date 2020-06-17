ed1f4bba8b1a04bfce7c60ef72b87d98d3ea167d
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-06-17 11:24:03 -0500

    Create Toko.svelte

---
+++ src/Toko.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Footer from "./widget/Footer.svelte";
+	import Navi from "./widget/Navi.svelte";
+</script>
+<Navi/>
+<div class="container p-5">
+	<div class="content">
+		<h1>Toko</h1>
+        <figure class="image is-16by9">
+            <iframe class="has-ratio" src="https://www.youtube.com/embed/VkwX5WpLRfc" title="aplikasi toko online shop whatsapp" allowfullscreen></iframe>
+          </figure>
+          <p>Sempurna adalah kata yang tepat untuk usaha toko shop store menggunakan aplikasi whatsapp order integrasi ini, dengan kemudahan kesempurnaan dalam bekerja serta dukungan fitur wow ini benar benar akan mempermudah pekerjaan mu, bukan sekedar toko online shop yang terintergasi dengan whatsapp saja tapi untuk paket premium sudah include dengan integrasi aplikasi kasir pos . cek laporan pendapatan semakin mudah dengan whatsapp shop untuk mendukung bisnis usaha mu.</p>
+		</div>
+  </div>
+<Footer/>
