92953ce850d76bea3b02bfbf8f2af91ea114a87c
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-06-18 11:25:02 -0500

    Create Profile.svelte

---
+++ src/Profile.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Footer from "./widget/Footer.svelte";
+	import Navi from "./widget/Navi.svelte";
+</script>
+<Navi/>
+<div class="container p-5">
+	<div class="content">
+		<h1>Fitur</h1>
+        <figure class="image is-4by3">
+            <iframe class="has-ratio" src="https://www.youtube.com/embed/l6fMXa5cmbA" title="aplikasi toko online shop whatsapp" allowfullscreen></iframe>
+          </figure>
+          <p>Fitur yang lengkap bisa kamu gunakan dalam whatsapp shop web app ini , penasaran apa saja fitur modules nya ?? cek video diatas dan pelajari mudah nya bekerja dengan aplikasi ini.</p>
+		  <p>Website dengan domain nama usaha bisnis mu, toko online shop dengan proses chekcout dan kalkulasi otomatis, system pemesanan akan handling masuk ke whatsapp kamu secara langsung, tata cara pembayaran page, dukungan QRIS QR Code payment untuk memudahkan pembayaran pelanggan , CMS Backend admin dengan login user name dan password, master data mengganti user dan password, grouping kategori produk,menu dan produk jual plus gambar, QR code upload QRIS digital payment mu, update halaman website detail, dan lain lain.</p>
+		  <p>Khusus paket premium include dengan aplikasi kasir point of sale penjualan kasir yang dilengkapi dengan laporan pendapatan detail plus laporan pembayaran pending order dan lain nya, dan kasbon pelanggan, untuk resto cafe include dengan bill payment dan pencatatan no meja .</p>
+		</div>
+  </div>
+<Footer/>
