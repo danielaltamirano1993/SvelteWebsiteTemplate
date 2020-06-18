27d98d3eaec2edd0afec5ef439d738fb2bba5390
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-06-18 11:24:26 -0500

    Create Resto.svelte

---
+++ src/Resto.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Footer from "./widget/Footer.svelte";
+	import Navi from "./widget/Navi.svelte";
+</script>
+<Navi/>
+<div class="container p-5">
+	<div class="content">
+		<h1>Resto</h1>
+        <figure class="image is-16by9">
+            <iframe class="has-ratio" src="https://www.youtube.com/embed/wuvIR7W34M4" title="aplikasi restoran cafe online shop whatsapp" allowfullscreen></iframe>
+          </figure>
+          <p>Dan akan selalu memudahkan pelaku usaha kuliner dalam penggunaan nya, khussus nya dalam kebutuhan handling order pelanggan secara online dengan fitur grouping menu , semakin cepat pelanggan memilih menu makanan dan minuman pada restoran cafe, plus dukungan kalkulasi otomatis dan handling order langsung ke whatsapp kamu , menyempurnakan mobilitas dalam bekerja. Play video demo dan pelajari begitu mudah nya penggunaan whatsapp resto cafe ini.</p>
+		</div>
+  </div>
+<Footer/>
