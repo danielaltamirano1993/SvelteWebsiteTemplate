d906e1d6554745eff2f13bb496799304488727b8
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-07-02 11:25:34 -0500

    Create Contact.svelte

---
+++ src/Contact.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Footer from "./widget/Footer.svelte";
+	import Navi from "./widget/Navi.svelte";
+</script>
+<Navi/>
+<div class="container p-5">
+	<div class="content">
+		<h1>Contact</h1>
+		<figure class="image is-16by9">
+            <img src="https://1.bp.blogspot.com/-3uU4-F2UHC0/YXBuFlUWq-I/AAAAAAAARaA/Ykp39LQzUDg1yUOIb_bsBD9jHYJFOQxXwCLcBGAsYHQ/s1920/16.jpg" alt="toko online whatsapp">
+          </figure>
+		  <p>Dan hubungi kami jika butuh bantuan informasi maupun konsultasi</p>
+		  <a href="https://wa.me/6285646104747" class="button is-primary">Whatsapp</a> <a href="tel:+62895339403223" class="button is-primary">Call Us</a> <a href="mailto:axcora@gmail.com" class="button is-primary">Email</a> 
+		</div>
+  </div>
+<Footer/>
