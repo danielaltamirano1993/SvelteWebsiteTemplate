5b60b0128c005ee5c3772952123ea11288f4eeb7
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-07-28 11:25:14 -0500

    Create Pricing.svelte

---
+++ src/Pricing.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Footer from "./widget/Footer.svelte";
+	import Navi from "./widget/Navi.svelte";
+</script>
+<Navi/>
+<div class="container p-5">
+	<div class="content">
+		<h1>Pricing Layanan</h1>
+        <figure class="image is-16by9">
+            <img src="https://whatshop.pages.dev/assets/img/15.jpg" alt="toko online whatsapp">
+          </figure>
+          <p>Harga layanan yang super murah untuk digunakan menunjang toko shop dan resto cafe mu dengan durasi layanan pertahun , wow makin murah pakek banget nih..</p>
+          <p>Basic Hanya Rp.410.000/tahun nya include dengan domain my id + whatsapp shop app + 300mb web space.</p>
+          <p>Standard Hanya Rp.610.000/tahun nya include dengan domain com + whatsapp shop app + 500mb web space.</p>
+          <p>Premium Hanya Rp.1.564.800/tahun nya include dengan domain com + whatsapp shop app + 1.500mb web space + Aplikasi POS kasir + APK Android.</p>
+          <p>Butuh bantuan ?? Klik tombol dibawah ini dan pilih layanan , segera mulai online kan bisnis mu.</p>
+          <a href="https://wa.me/6285646104747" class="button is-primary">Whatsapp</a> <a href="tel:+62895339403223" class="button is-primary">Call Us</a> <a href="mailto:axcora@gmail.com" class="button is-primary">Email</a> 
+        </div>
+  </div>
+<Footer/>
