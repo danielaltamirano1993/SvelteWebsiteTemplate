59bce5e0e1d432d5489345c6a9ce902b874a5b5f
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-10-19 11:26:11 -0500

    Create Beranda.svelte

---
+++ src/Beranda.svelte
@@ -0,0 +1,90 @@
+<script>
+	import Footer from "./widget/Footer.svelte";
+</script>
+<section class="hero is-success is-fullheight">
+	<div class="hero-head">
+	  <header class="navbar">
+		<div class="container">
+		</div>
+	  </header>
+	</div>
+  	<div class="hero-body">
+	  <div class="container has-text-centered">
+		<p class="title"> Whatsapp <br/><i class="fab fa-whatsapp fa-4x"></i><br/> Shop
+		</p>
+		<p class="subtitle">
+		  Online Order Whatsapp Shop
+		</p>
+	  </div>
+	</div>
+  </section>
+<section class="section">
+    <div class="container">
+      <h1 class="title">
+       Toko Online Whatsapp
+      </h1>
+      <p class="subtitle">
+        Kini saat nya manjakan dirimu dengan toko online shop order intergasi whatsapp
+      </p>
+	  <div class="tile is-ancestor">
+		<div class="tile is-vertical is-8">
+		  <div class="tile">
+			<div class="tile is-parent is-vertical">
+			  <article class="tile is-child notification is-primary">
+				<p class="title">Shop</p>
+				<figure class="image is-4by3">
+				<img src="https://whatshop.vercel.app/static/a7886685025b75097793170323414066/9f7bd/shop.avif" alt="toko online whatsapp"/>
+			</figure>
+				<p class="subtitle">Toko online shop ready dengan domain mu.</p>
+			  </article>
+			  <article class="tile is-child notification is-warning">
+				<p class="title">CMS</p>
+				<figure class="image is-4by3">
+				<img src="https://whatshop.vercel.app/static/2b3a77900025009d10effbae5ed33745/9f7bd/wo.avif" alt="toko online whatsapp"/>
+			</figure>
+				<p class="subtitle">Content Management System Web admin dashboard.</p>
+			  </article>
+			</div>
+			<div class="tile is-parent">
+			  <article class="tile is-child notification is-info">
+				<p class="title">Whatsapp</p>
+				<p class="subtitle">Integrasi dengan whatsapp mu.</p>
+				<figure class="image is-4by3">
+				  <img src="https://whatshop.vercel.app/static/568c9beabe4e610d0ec43592df11d74c/9f7bd/webs.avif" alt="toko online whatsapp">
+				</figure>
+				<p class="subtitle">Toko online shop yang dirancang khusus untuk whatsapp , kemudahan kalkulasi order otomatis hingga proses transaksi atau tata cara pembayaran yang lengkap memudahkan pelanggan untuk order dengan cepat dengan proses checkout ke whatapp mu langsung.</p>
+				<p class="subtitle">Include dengan dukungan nama domain untuk usaha dan bisnis mu , semakin sempurna dalam menggunakan whatsapp shop ini.</p>
+			  </article>
+			</div>
+		  </div>
+		  <div class="tile is-parent">
+			<article class="tile is-child notification is-success">
+			  <p class="title">All in one Apps</p>
+			  <p class="subtitle">Dan semua nya di hanlde dalam satu applikasi saja untuk mu.</p>
+			  <div class="content">
+				<!-- Content -->
+			  </div>
+			</article>
+		  </div>
+		</div>
+		<div class="tile is-parent">
+		  <article class="tile is-child notification is-danger">
+			<div class="content">
+			  <p class="title">A Premium</p>
+			  <p class="subtitle">Sempurnakan dengan POS Apps.</p>
+			  <figure class="image is-4by3">
+				<img src="https://whatshop.vercel.app/static/a6e48324a165407847d89e3bd09b5079/9f7bd/qris.avif" alt="toko online whatsapp">
+			  </figure>
+			  <p class="subtitle">Integrasi dengan point of sale apps, aplikasi kasir toko dan restoran dan disesuaikan dengan bidang bisnis mu terintegrasi secara sempurna dengan whatsapp shop akan menjadi lebih baik dalam proses pencatatan pembukuan, melakukan transaksi kasir dengan mudah dan cepat via mesin kasir plus cetak struk kasir serta monitoring omset pendapatan detail akan mendukung mobilitas mu bekerja , dengan whatshop satu aplikasi untuk semua nya.</p>
+			  <p class="subtitle">Saat nya kini revolusi bisnis mu dengan whatshop aplikasi lengkap dan murah untuk mu.</p>
+			  <div class="content">
+				<!-- Content -->
+			  </div>
+			</div>
+		  </article>
+		</div>
+	  </div>
+	</div>
+  </section>
+
+<Footer/>
