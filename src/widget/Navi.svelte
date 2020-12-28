66133b32bb6372b851461fe7e23fff63445ae004
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-12-28 11:27:35 -0500

    Create Navi.svelte

---
+++ src/widget/Navi.svelte
@@ -0,0 +1,10 @@
+<section class="hero is-medium is-success has-text-centered">
+	<div class="hero-body">
+	  <p class="title">
+		<a href="/">Whatsapp <i class="fab fa-whatsapp"></i> Shop</a>
+	  </p>
+	  <p class="subtitle">
+		Toko Online Shop Whatsapp Order
+	  </p>
+	</div>
+  </section>
