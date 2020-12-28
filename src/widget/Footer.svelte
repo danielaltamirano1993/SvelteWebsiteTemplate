a189d9cb3006f25f26ed76bece344229c7b8f12c
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-12-28 11:27:23 -0500

    Create Footer.svelte

---
+++ src/widget/Footer.svelte
@@ -0,0 +1,10 @@
+<footer class="footer">
+	<div class="content has-text-centered">
+	  <p>
+		<strong><a href="/">Whatsapp Shop</a></strong><br/><small> 
+        built with <a href="https://svelte.dev">svelte</a> feat <a href="https://bulma.io">bulma</a> 
+		Repo on <a href="https://github.com/">github</a> Host by <a href="https://vercel.com">Vercel</a>.
+    </small>
+	  </p>
+	</div>
+  </footer>
